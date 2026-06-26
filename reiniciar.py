a237d421562bc4263a46572911cfac95345a575d
ibarra galvan luis angel <baryplox@gmail.com>
2026-06-26 13:40:55 -0700

    Refactor database creation to avoid deletion

--- reiniciar.py
+++ reiniciar.py
@@ -1,33 +1,22 @@
 import sqlite3
-import os
 
-def limpiar_y_crear_bd():
+def crear_bd():
     archivo_bd = "sistema_escolar.db"
     
-
-    if os.path.exists(archivo_bd):
-        try:
-            os.remove(archivo_bd)
-            print(" Base de datos vieja eliminada con exito.")
-        except PermissionError:
-            print("ERROR: Cierra los archivos 'main.py' o 'app.py' antes de ejecutar esto.")
-            return
-
-  
     conexion = sqlite3.connect(archivo_bd)
     cursor = conexion.cursor()
 
     cursor.execute("""
-    CREATE TABLE alumnos (
+    CREATE TABLE IF NOT EXISTS alumnos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nombre TEXT NOT NULL,
         grado TEXT NOT NULL,
         correo_maestra TEXT NOT NULL
     )
     """)
-
+    
     cursor.execute("""
-    CREATE TABLE padres (
+    CREATE TABLE IF NOT EXISTS padres (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         alumno_id INTEGER,
         nombre TEXT NOT NULL,
@@ -39,7 +28,8 @@ def limpiar_y_crear_bd():
     
     conexion.commit()
     conexion.close()
-    print("Estructura de la base de datos creada desde cero con exito")
+    print("Base de datos y estructuras creadas con exito.")
 
 if __name__ == "__main__":
-    limpiar_y_crear_bd()
+    crear_bd()
+
