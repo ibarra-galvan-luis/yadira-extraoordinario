16b984fbfd2c4a2df370e29aeabca7b20aeca6d6
ibarra galvan luis angel <baryplox@gmail.com>
2026-06-26 13:25:19 -0700

    Add files via upload

---
+++ reiniciar.py
@@ -0,0 +1,45 @@
+import sqlite3
+import os
+
+def limpiar_y_crear_bd():
+    archivo_bd = "sistema_escolar.db"
+    
+
+    if os.path.exists(archivo_bd):
+        try:
+            os.remove(archivo_bd)
+            print(" Base de datos vieja eliminada con éxito.")
+        except PermissionError:
+            print("ERROR: Cierra los archivos 'main.py' o 'app.py' antes de ejecutar este script.")
+            return
+
+  
+    conexion = sqlite3.connect(archivo_bd)
+    cursor = conexion.cursor()
+
+    cursor.execute("""
+    CREATE TABLE alumnos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        grado TEXT NOT NULL,
+        correo_maestra TEXT NOT NULL
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE padres (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        alumno_id INTEGER,
+        nombre TEXT NOT NULL,
+        correo TEXT NOT NULL,
+        foto_ruta TEXT NOT NULL,
+        FOREIGN KEY (alumno_id) REFERENCES alumnos (id) ON DELETE CASCADE
+    )
+    """)
+    
+    conexion.commit()
+    conexion.close()
+    print("Estructura de la base de datos creada desde cero con exito")
+
+if __name__ == "__main__":
+    limpiar_y_crear_bd()
